b71b7a6c0a1ab1860d412845b54c11e46b445e86
ONYX-Minestudio <onix.launcher@outlook.com>
2025-06-13 20:33:01 +0200

    distribution.json maintenant reconnue
    Le fichier "distribution.json" est bien reconnu mais il y a des errors à régler

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://www.dropbox.com/scl/fi/yy67nehx7tidx4f6tazlr/distribution.json?rlkey=6a0gwfrm4xym9cv8exzo2pjpi&st=dtv5wl2d&dl=1'
+exports.REMOTE_DISTRO_URL = 'https://onyx-studio.ct.ws/nebula/distribution.json?i=1 '
 // exports.np_empire = 'http://www.onyx-studio.ct.ws/nebula/distribution.json'
 
 const api = new DistributionAPI(
