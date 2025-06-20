46baa40492c4881d97393f43a50478ca64958dfc
ONYX-Minestudio <onix.launcher@outlook.com>
2025-06-20 18:46:57 +0200

    launcher fonctionnel + license mis à jour

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -6,7 +6,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://localhost:3000/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://onyx-studio.ct.ws/np-empire/distribution.json'
 // exports.np_empire = 'http://www.onyx-studio.ct.ws/nebula/distribution.json'
 
 const api = new DistributionAPI(
